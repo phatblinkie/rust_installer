dc5806a81c99d47b49d61359dff04bea4f4547c7
Brian Bowen <bbowen@scholz-associates.com>
2022-04-20 17:07:11 -0500

    Update install_rust.sh

--- install_rust.sh
+++ install_rust.sh
@@ -14,11 +14,13 @@ if ( test -f /usr/bin/apt ) ; then apt-get install -y glibc.i686 libstdc++.i686 
 
 mkdir -p steaminstaller
 cd steaminstaller
+#clean up unwanted trash
+rm -rf /tmp/dumps
 #download and unpack steamcmd
 curl -sqL "https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz" | tar zxvf -
 
 #this updates the steam client
-./steamcmd.sh
+./steamcmd.sh +quit
 
 #move steamfiles to /usr/local/bin so its in the right spot for users bin
 mv * /usr/local/bin/
@@ -36,16 +38,14 @@ echo created username rust for service
 fi
 
 #change to the user for the rust install (not strictly needed but if you got the disk space why not)
-sudo su - rust
-
 #install the linux rust server files (vanilla, public branch), anon login, and force to users home/rustserver/ dir (+exit gets out of steamcmd, leave it there)
-/usr/local.bin/steamcmd.sh +force_install_dir ~/rustserver/ +login anonymous +app_update 258550 validate +exit
+sudo -u rust /usr/local/bin/steamcmd.sh +force_install_dir ~/rustserver/ +login anonymous +app_update 258550 validate +exit
 
 #will take a few minutes to download
 # the service file will update oxide when run.
 
-wget --output-file=/usr/lib/systemd/system/rust.service https://raw.githubusercontent.com/phatblinkie/rust_installer/main/rust.service
-wget --output-file=/usr/local/bin/start_rust.sh https://raw.githubusercontent.com/phatblinkie/rust_installer/main/start_rust.sh
+wget --output-document=/usr/lib/systemd/system/rust.service https://raw.githubusercontent.com/phatblinkie/rust_installer/main/rust.service
+wget --output-document=/usr/local/bin/start_rust.sh https://raw.githubusercontent.com/phatblinkie/rust_installer/main/start_rust.sh
 
 chmod 0755 /usr/local/bin/start_rust.sh
 systemctl daemon-reload
